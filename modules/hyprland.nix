b3fb791bfe137bf4d36c41656a4a2e16f83ba679
guhnope <dane.butler@gmail.com>
2026-06-09 18:30:44 +1000

    missed close bracket

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -11,6 +11,7 @@
           xdg-desktop-portal-gtk
         ];
         config.common.default = [ "hyprland" "gtk" ];
+    };
     environment.sessionVariables = {
       QT_QPA_PLATFORMTHEME = "qt6ct";
       GTK_THEME = "adw-gtk3-dark";
